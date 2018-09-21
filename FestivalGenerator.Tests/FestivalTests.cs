edc2a3a5351503c5af3e362b5113b540c9370ff1
Danielle Dias <danielle.dias@computershare.co.uk>
2018-09-21 15:35:25 +0100

    Added unit tests for Day and Date Calculators

--- FestivalGenerator.Tests/FestivalTests.cs
+++ FestivalGenerator.Tests/FestivalTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FestivalGenerator;
 
 namespace FestivalGenerator.Tests
 {
@@ -9,6 +10,40 @@ namespace FestivalGenerator.Tests
         [TestMethod]
         public void StartDayCalculator_ValidDate_ReturnsCorrectDayOfWeek()
         {
+            //Arrange
+            Festival festival = new Festival();
+            var expectedResult = new DateTime(2094, 02, 02).DayOfWeek;
+            //Act
+            var actualResult = festival.StartDayCalculator(new DateTime(2094,02,02));
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EndDayCalculator_ValidDate_ReturnsCorrectEndDay()
+        {
+            //Arrange
+            Festival festival = new Festival();
+            var expectedResult = new DateTime(2094, 01, 03).DayOfWeek;
+            //Act
+            var actualResult = festival.EndDayCalculator(new DateTime(2094, 01, 01), 2); 
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EndDateCalculator_ValidDate_ReturnsCorrectEndDate()
+        {
+            //Arrange
+            Festival festival = new Festival();
+            var expectedResult = new DateTime(2094, 01, 03);
+            //Act
+            var actualResult = festival.EndDateCalculator(new DateTime(2094, 01, 01), 2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
