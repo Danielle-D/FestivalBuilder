5b025e825455eaad9384d963b5ba56eb7e47ac7b
Danielle Dias <danielle.dias@computershare.co.uk>
2018-09-21 12:43:46 +0100

    Added FestivalGenerator.Tests

---
+++ FestivalGenerator.Tests/FestivalTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FestivalGenerator;
+
+namespace FestivalGenerator.Tests
+{
+    [TestClass]
+    public class FestivalTests
+    {
+        [TestMethod]
+        public void StartDayCalculator_ValidDate_ReturnsCorrectDayOfWeek()
+        {
+
+        }
+
+    }
+}
