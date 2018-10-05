8947ef1e16a85c28b3964f7460fa448dcbd371b5
Danielle Dias <danielle.dias@computershare.co.uk>
2018-10-05 11:51:52 +0100

    Added NUnit to use the Set Up Attribute.

--- FestivalGenerator.Tests/FestivalTests.cs
+++ FestivalGenerator.Tests/FestivalTests.cs
@@ -1,48 +1,48 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FestivalGenerator;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace FestivalGenerator.Tests
 {
     [TestClass]
     public class FestivalTests
     {
-        [TestMethod]
+        private Festival festival;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.festival = new Festival();
+        }
+
+        [Test]
         public void StartDayCalculator_ValidDate_ReturnsCorrectDayOfWeek()
         {
-            //Arrange
-            Festival festival = new Festival();
             var expectedResult = new DateTime(2094, 02, 02).DayOfWeek;
-            //Act
+
             var actualResult = festival.StartDayCalculator(new DateTime(2094,02,02));
 
-            //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
 
-        [TestMethod]
+        [Test]
         public void EndDayCalculator_ValidDate_ReturnsCorrectEndDay()
         {
-            //Arrange
-            Festival festival = new Festival();
             var expectedResult = new DateTime(2094, 01, 03).DayOfWeek;
-            //Act
+
             var actualResult = festival.EndDayCalculator(new DateTime(2094, 01, 01), 2); 
 
-            //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
 
-        [TestMethod]
+        [Test]
         public void EndDateCalculator_ValidDate_ReturnsCorrectEndDate()
         {
-            //Arrange
-            Festival festival = new Festival();
             var expectedResult = new DateTime(2094, 01, 03);
-            //Act
+
             var actualResult = festival.EndDateCalculator(new DateTime(2094, 01, 01), 2);
 
-            //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
     }
